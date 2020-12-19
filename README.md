a8a07e1a3ad01c63abc45e7711c0f33ac7c54a7b
JuanfeRomero <juanferomerocode@gmail.com>
2020-12-18 21:37:09 -0300

    added explanations to the README file and added the linkedList.js file, where I practice creating a linked list in js through recursion

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JavascriptPractice
 Here I will include practice code made in JS
+
+## Exercises:
+- **primeNumber.js** : This code shows a function that detects if any number is a prime number, then provides an array of number for testing and returns if each of those number is or isn't a prime number.
+- **linkedList.js** : This code shows a way of interpreting the concept of linked lists in Javascript. It creates a Node Object with a Data and a Next Value, and a function Solution which utilizes the nodes to either add a new node to the current instanced object through recursion, or display the solution showing the current state of the node and access each of its data value. It also uses the *JSON* method *stringify* to format the node object in a more aestethic way.
---
+++ linkedList.js
@@ -0,0 +1,46 @@
+function Node(data) {
+    this.data = data;
+    this.next = null;
+}
+function Solution() {
+    this.insert = function (head, data) {
+        let newNode = new Node(data);
+        if (head === null) {
+            head = newNode;
+            return head;
+        }
+        let header = head;
+        if (head.next === null) {
+            head.next = newNode;
+            return head;
+        } else {
+            let buffer = this.insert(head.next, data);
+            header.next = buffer;
+            return header;
+        }
+    };
+
+    this.display = function (head) {
+        var start = head;
+        console.log(JSON.stringify(head, null, 2));
+        while (start) {
+            console.log(start.data + ' ');
+            start = start.next;
+        }
+    };
+}
+
+const testBatch = [21, 12, 18, 2020, 3, 224];
+function main() {
+    console.log('entre a main');
+    var T = testBatch;
+    var head = null;
+    var mylist = new Solution();
+    for(i = 0; i < T.length-1; i++){
+        var data = T[i];
+        head = mylist.insert(head, data);
+    }
+    mylist.display(head);
+}
+
+main();
